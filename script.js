0607d5f572fe7113e31c251127285a1f345e393a
Tarun Kumar Raj <rajtarunkumar007@gmail.com>
2022-08-23 11:10:47 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () =>{
+        nav.classList.add('active');
+    })
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
+
+// Single Page Product Details....
+
+var mainimg = document.getElementById('MainImg');
+var smallimg = document.getElementsByClassName('small-img');
+
+smallimg[0].onclick = function(){
+    mainimg.src = smallimg[0].src;
+}
+
+smallimg[1].onclick = function(){
+    mainimg.src = smallimg[1].src;
+}
+
+smallimg[2].onclick = function(){
+    mainimg.src = smallimg[2].src;
+}
+
+smallimg[3].onclick = function(){
+    mainimg.src = smallimg[3].src;
+}
